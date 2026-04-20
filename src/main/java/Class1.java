4cc5ac2dfb702a2aad632960d17c7712a90a955a
Triikk <ronchimattia03@gmail.com>
2026-04-20 11:34:03 +0200

    added public to main

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,5 +1,5 @@
 public class Class1 {
-    static void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello, World!");
     }
 }
--- src/main/java/Class3.java
+++ src/main/java/Class3.java
@@ -1,5 +1,5 @@
 public class Class3 {
-    static void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello, World3!");
     }
 }
